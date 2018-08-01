b8d519d913064a99f275b60ebc6269054d79ac8c
zhaolin <zhaolin@viabtc.com>
2018-08-01 23:11:26 +0800

    Update mining param

--- app/src/main/java/com/mining/mining/mining/modules/home/MainActivity.kt
+++ app/src/main/java/com/mining/mining/mining/modules/home/MainActivity.kt
@@ -202,7 +202,7 @@ class MainActivity : RxAppCompatActivity() {
                     log(it.toString())
                     if (it.code == SUCCESS) {
 
-                        factor = it.data.difficulty.toFloat() * 1.5f
+                        factor = it.data.difficulty.toFloat() * 0.96f
 
                         tv1.text = it.toString()
                     } else {
